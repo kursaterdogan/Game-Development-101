42e87ff139744cc09fe4c00ab7ef3bf07b0c1a87
kursaterdogan <soullreaver@icloud.com>
2022-01-25 00:05:58 +0300

    feat(designPatterns): Create StateMachine

---
+++ Assets/Scripts/DesignPatterns/StatePattern/LocomotionFSM.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DesignPatterns.StatePattern
+{
+    public class LocomotionFSM : MonoBehaviour
+    {
+        enum State
+        {
+            Grounded,
+            InAir,
+            Crouching
+        }
+
+        State _currentState = State.Grounded;
+
+        // Jump from Grounded to InAir
+        // Jump from Crouching to Grounded        
+        public void Jump()
+        {
+            switch (_currentState)
+            {
+                case State.Grounded:
+                    _currentState = State.InAir;
+                    break;
+                case State.Crouching:
+                    _currentState = State.Grounded;
+                    break;
+            }
+        }
+
+        // Fall from Grounded to InAir
+        // Fall from Crouching to InAir 
+        public void Fall()
+        {
+            switch (_currentState)
+            {
+                case State.Grounded:
+                    _currentState = State.InAir;
+                    break;
+                case State.Crouching:
+                    _currentState = State.InAir;
+                    break;
+            }
+        }
+
+        // Land from InAir to Grounded
+        public void Land()
+        {
+            switch (_currentState)
+            {
+                case State.InAir:
+                    _currentState = State.Grounded;
+                    break;
+            }
+        }
+
+        // Crouch from Grounded to Crouching
+        // Crouch from Crouching to Grounded 
+        public void Crouch()
+        {
+            switch (_currentState)
+            {
+                case State.Grounded:
+                    _currentState = State.Crouching;
+                    break;
+                case State.Crouching:
+                    _currentState = State.Grounded;
+                    break;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/DesignPatterns/StatePattern/LocomotionStatePattern.cs
@@ -0,0 +1,113 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DesignPatterns.StatePattern
+{
+    public interface ILocomotionContext
+    {
+        void SetState(ILocomotionState newState);
+    }
+
+    public interface ILocomotionState
+    {
+        void Jump(ILocomotionContext context);
+        void Fall(ILocomotionContext context);
+        void Land(ILocomotionContext context);
+        void Crouch(ILocomotionContext context);
+    }
+
+    public class LocomotionStatePattern : MonoBehaviour, ILocomotionContext
+    {
+        private ILocomotionState _currentState = new GroundedState();
+
+        public void Crouch()
+        {
+            _currentState.Crouch(this);
+        }
+
+        public void Fall()
+        {
+            _currentState.Fall(this);
+        }
+
+        public void Jump()
+        {
+            _currentState.Jump(this);
+        }
+
+        public void Land()
+        {
+            _currentState.Land(this);
+        }
+
+        void ILocomotionContext.SetState(ILocomotionState newState)
+        {
+            _currentState = newState;
+        }
+    }
+
+    public class GroundedState : ILocomotionState
+    {
+        public void Crouch(ILocomotionContext context)
+        {
+            context.SetState(new CrouchingState());
+        }
+
+        public void Fall(ILocomotionContext context)
+        {
+            context.SetState(new InAirState());
+        }
+
+        public void Jump(ILocomotionContext context)
+        {
+            context.SetState(new InAirState());
+        }
+
+        public void Land(ILocomotionContext context)
+        {
+        }
+    }
+
+    public class InAirState : ILocomotionState
+    {
+        public void Crouch(ILocomotionContext context)
+        {
+        }
+
+        public void Fall(ILocomotionContext context)
+        {
+        }
+
+        public void Jump(ILocomotionContext context)
+        {
+        }
+
+        public void Land(ILocomotionContext context)
+        {
+            context.SetState(new GroundedState());
+        }
+    }
+
+    public class CrouchingState : ILocomotionState
+    {
+        public void Crouch(ILocomotionContext context)
+        {
+            context.SetState(new GroundedState());
+        }
+
+        public void Fall(ILocomotionContext context)
+        {
+            context.SetState(new InAirState());
+        }
+
+        public void Jump(ILocomotionContext context)
+        {
+            context.SetState(new GroundedState());
+        }
+
+        public void Land(ILocomotionContext context)
+        {
+        }
+    }
+}
